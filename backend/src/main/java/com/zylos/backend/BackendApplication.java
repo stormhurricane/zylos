c5010414d6f081151f140487fca99e74f92b1fd9
stormhurricane <xstormhurricane@gmail.com>
2026-06-24 22:23:21 +0200

    excluded old code from BackendApplication start

--- backend/src/main/java/com/zylos/backend/BackendApplication.java
+++ backend/src/main/java/com/zylos/backend/BackendApplication.java
@@ -2,12 +2,32 @@ package com.zylos.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 // @EnableScheduling
-@EnableAsync
+@ComponentScan(
+    basePackages = {
+        "com.zylos.backend.config",
+        "com.zylos.backend.exception",
+        "com.zylos.backend.features"
+    }
+)
+@EnableJpaRepositories(
+    basePackages = {
+        "com.zylos.backend.features" 
+    }
+)
+@EntityScan(
+    basePackages = {
+        "com.zylos.backend.features" 
+    }
+)
+// @EnableAsync
 public class BackendApplication {
 
     public static void main(String[] args) { SpringApplication.run(BackendApplication.class, args);
