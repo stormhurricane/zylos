7f6e6e052a7f3252b908fb40b7ef22aa3d3e1174
stormhurricane <xstormhurricane@gmail.com>
2026-06-28 15:17:15 +0200

    improved features.user repositories

--- backend/src/main/java/com/zylos/backend/features/user/UserRepository.java
+++ backend/src/main/java/com/zylos/backend/features/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.zylos.backend.features.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
+    @Query("SELECT u FROM User u " +
+           "LEFT JOIN FETCH Student s ON u.id = s.id " +
+           "LEFT JOIN FETCH Teacher t ON u.id = t.id " +
+           "WHERE LOWER(u.firstName) LIKE LOWER(CONCAT('%', :firstName, '%')) " +
+           "OR LOWER(u.lastName) LIKE LOWER(CONCAT('%', :lastName, '%'))")
     List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
