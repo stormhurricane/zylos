54e18b4d8236e1a40746a80e442b61b7c4719c64
stormhurricane <xstormhurricane@gmail.com>
2026-09-16 18:06:46 +0200

    reduced App.tsx to simple Login Component

---
+++ frontend/src/features/auth/components/Login.tsx
@@ -0,0 +1,17 @@
+function Login() {
+    return ( 
+      <div >
+        <div >
+          <label>Kennung</label>
+          <input type="text"></input>
+        </div>
+        <div >
+          <label>Passwort</label>
+          <input type="password"></input>
+        </div>
+        <button>Login</button>
+      </div>
+    );
+}
+
+export default Login;
