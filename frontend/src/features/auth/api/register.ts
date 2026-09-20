dbec8ffa1e66eda5c0a3096fd226e103e53a3b4e
stormhurricane <xstormhurricane@gmail.com>
2026-09-20 11:59:08 +0200

    frontend: added api stub for registration

---
+++ frontend/src/features/auth/api/register.ts
@@ -0,0 +1,6 @@
+import type { RegisterFormData } from "../../../types/auth";
+
+export const registerUser = async (formData: RegisterFormData) => {
+    console.log(formData);
+    console.log("Registering user...");
+};
