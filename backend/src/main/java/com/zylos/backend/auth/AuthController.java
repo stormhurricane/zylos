02c91fdc1d86539ba0dd7e0525f94aa8722854ad
stormhurricane <xstormhurricane@gmail.com>
2026-09-26 11:30:25 +0200

    added validation for registration body

--- backend/src/main/java/com/zylos/backend/auth/AuthController.java
+++ backend/src/main/java/com/zylos/backend/auth/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zylos.backend.auth.dto.RegisterRequest;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,7 +20,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping ("/register")
-    public void register(@RequestBody RegisterRequest request) {
+    public void register(@Valid @RequestBody RegisterRequest request) {
         log.debug("Registering user: {}", request);
         authService.register(request);
     }
--- backend/src/main/java/com/zylos/backend/auth/dto/RegisterRequest.java
+++ backend/src/main/java/com/zylos/backend/auth/dto/RegisterRequest.java
@@ -1,9 +1,28 @@
 package com.zylos.backend.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
+    
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, message = "First name must be at least 2 characters long")
     String firstName,
+    
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, message = "Last name must be at least 2 characters long")
     String lastName,
+    
+    @NotBlank(message = "Email is required")
+    @Email (message = "Email should be valid")
     String email,
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     String password,
+    
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, message = "Username must be at least 3 characters long")
     String username
 ) {}
