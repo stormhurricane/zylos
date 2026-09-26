07b8300dc9928554aeeb3acddbc60d3b8c7668d8
stormhurricane <xstormhurricane@gmail.com>
2026-09-26 11:26:51 +0200

    added empty authservice

--- backend/src/main/java/com/zylos/backend/auth/AuthController.java
+++ backend/src/main/java/com/zylos/backend/auth/AuthController.java
@@ -7,16 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zylos.backend.auth.dto.RegisterRequest;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@AllArgsConstructor 
 @Slf4j 
 @RestController 
 @RequestMapping ("api/v1/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping ("/register")
     public void register(@RequestBody RegisterRequest request) {
-        log.info("Registering user: {}", request);
+        log.debug("Registering user: {}", request);
+        authService.register(request);
     }
     
 }
---
+++ backend/src/main/java/com/zylos/backend/auth/AuthService.java
@@ -0,0 +1,15 @@
+package com.zylos.backend.auth;
+
+import org.springframework.stereotype.Service;
+
+import com.zylos.backend.auth.dto.RegisterRequest;
+
+@Service 
+public class AuthService {
+
+    
+    public void register(RegisterRequest request) {
+        // Implement registration logic here
+    }
+    
+}
